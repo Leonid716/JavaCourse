702b70c474eab671619f9566d917e96fc668f9a6
student <student@localhost>
2019-03-05 10:32:54 +0300

    Program optimization

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-[![Build Status](https://travis-ci.org/Leonid716/JavaCourse.svg?branch=master)](https://travis-ci.org/Leonid716/JavaCourse) [![codecov](https://codecov.io/gh/Leonid716/JavaCourse/branch/master/graph/badge.svg)](https://codecov.io/gh/Leonid716/JavaCourse)
+[![Build Status](https://travis-ci.org/Leonid716/JavaCourse.svg?branch=master)](https://travis-ci.org/Leonid716/SportmasterBonus) [![codecov](https://codecov.io/gh/Leonid716/SportmasterBonus/branch/master/graph/badge.svg)](https://codecov.io/gh/Leonid716/SportmasterBonus)
 
 # Bonus program
--- src/main/java/sportmasterBonus/BonusProgram.java
+++ src/main/java/sportmasterBonus/BonusProgram.java
@@ -3,17 +3,19 @@ package sportmasterBonus;
 public class BonusProgram {
     public int calculate(int currentPurchase, int totalPurchases) {
 
+        int bonusForEveryThousand = currentPurchase / 1000;
+
         if (totalPurchases > 0 && totalPurchases <= 15_000) {
-            return currentPurchase / 1000 * 50;
+            return bonusForEveryThousand * 50;
         }
         if (totalPurchases > 15_000 && totalPurchases < 150_000) {
-            return currentPurchase / 1000 * 70;
+            return bonusForEveryThousand * 70;
         }
         if (totalPurchases >= 150_000) {
-            return currentPurchase / 1000 * 100;
+            return bonusForEveryThousand * 100;
 
         }
-        return currentPurchase;
+        return 0;
     }
 }
 
